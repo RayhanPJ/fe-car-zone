31b35b6799a2507d1b8c6c9fb9819b79b3f83ec1
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-02 16:51:02 +0700

    create main layout

---
+++ src/app/(public)/page.jsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button"
+
+const HomePage = () => {
+  return (
+    <>
+     {/* <div className='main-container flex flex-wrap gap-2'>
+         <Button variant="default">
+           default
+         </Button>
+         <Button variant="outline">
+           outline
+         </Button>
+         <Button variant="secondary">
+           secondary
+         </Button>
+         <Button variant="ghost">
+           ghost
+         </Button>
+         <button 
+           className='btn btn-success'
+           type="button">
+           success class
+         </button>
+         <Button variant="success">
+           success
+         </Button>
+         <button 
+           className='btn btn-destructive'
+           type="button">
+           destructive class
+         </button>
+         <Button variant="destructive">
+           destructive
+         </Button>
+     </div>
+     <h1>Home page</h1> */}
+    
+    </>
+  )
+}
+
+export default HomePage
