731bce59292007dac7ab97c9a29ac09e35621ffc
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-04 06:33:14 +0700

    add theme toggler to sign in and sign up page

--- src/components/common/ThemeToggler.jsx
+++ src/components/common/ThemeToggler.jsx
@@ -5,8 +5,9 @@ import { Sun, Moon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
+import { cn } from '@/lib/utils'
 
-export default function ThemeToggler() {
+export default function ThemeToggler({ className, ...props }) {
   const { setTheme, resolvedTheme } = useTheme()
   const [isDark, setIsDark] = useState(false)
   const [mounted, setMounted] = useState(false)
@@ -30,7 +31,7 @@ export default function ThemeToggler() {
   }
 
   return (
-    <Button variant="ghost" onClick={() => setTheme(isDark ? "light" : "dark")}>
+    <Button variant="ghost" className={cn(className)} onClick={() => setTheme(isDark ? "light" : "dark")} {...props}>
       {isDark ? <Sun /> : <Moon />}
     </Button>
   )
