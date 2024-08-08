29a1f031fde3fec3aa12619d5a0eb2727c9c0878
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-09 04:37:56 +0700

    add alert on buy btn

--- src/app/(public)/cars/_components/BuyBtn.jsx
+++ src/app/(public)/cars/_components/BuyBtn.jsx
@@ -2,14 +2,25 @@
 import { Button } from "@/components/ui/button"
 import { usePayment } from "../../checkout/_components/usePayment"
 import { useRouter } from "next/navigation"
+import { useAuth } from "@/hooks/useAuth"
+import Swal from "sweetalert2"
 
 const BuyBtn = ({ car_id = null }) => {
    const { setCarID } = usePayment()
    const router = useRouter()
+   const { auth } = useAuth()
 
    const handleClick = () => {
-    setCarID(car_id)  
-    router.push("/checkout")
+    if(auth?.token){
+      setCarID(car_id)  
+      router.push("/checkout")
+    }else{
+      Swal.fire({
+         title: "Account required!",
+         text: "You have to use your account before buy this car",
+         icon: 'error'
+      })
+    }
    }
 
    return (
