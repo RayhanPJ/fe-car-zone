82be0eb9b299d144420187e2fb76cf114a2ac335
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-09 05:30:13 +0700

    add overview cards & fix delete car

--- src/app/(admin)/dashboard/cars/[id]/page.jsx
+++ src/app/(admin)/dashboard/cars/[id]/page.jsx
@@ -1,29 +1,36 @@
 import BreadCrumb from "@/components/common/BreadCrumb"
 import UpdateForm from "./_components/UpdateForm"
 import { Card } from "@/components/ui/card"
+import { API_BASE_URL } from "@/constants/variables"
 
-export const metadata = {
-   title: "Update Car | Dashboard Carzone"
+const getCarByID = async (id) => {
+  const req = await fetch(API_BASE_URL + "/api/cms/cars/" + id,  { cache: "no-store" })
+  return await req.json()
 }
 
-
-
-
-const UpdateCarsPage = () => {
+const UpdateCarsPage = async ({ params, searchParams }) => {
   return (
   <>
     <div className="flex flex-col md:flex-row items-center justify-between gap-2 py-10 px-3">
       <div className="">
-        <h1 className="text-2xl font-bold">Update Car Data</h1>
+        <h1 className="text-2xl font-bold">{searchParams?.detail ? `Detail Car data` : "Update Car Data"}</h1>
         <BreadCrumb />
       </div>
     </div>
 
     <Card className="p-5 min-h-fit overflow-x-auto">
-      <UpdateForm />
+      <UpdateForm carID={params.id} />
     </Card>
   </>
   )
+}
+
+export const generateMetadata = async ({ params, searchParams }) => {
+  const data = await getCarByID(params.id)
+
+  return {
+    title: searchParams?.detail ? `Detail "${data?.car.name}"` : `Update "${data?.car.name}"` 
+  }
 }
 
 export default UpdateCarsPage
--- src/app/(admin)/dashboard/page.jsx
+++ src/app/(admin)/dashboard/page.jsx
@@ -1,48 +1,12 @@
 import { Card, CardHeader, CardDescription, CardTitle, CardContent, CardFooter } from "@/components/ui/card"
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell, TableFooter } from "@/components/ui/table"
-import { Badge } from "@/components/ui/badge"
 import Link from "next/link"
 import OverviewChart from "./_components/OverviewChart"
+import OrdersTable from "./_components/OrdersTable"
+import { CarsOverview, UsersOverview, BrandsOverview } from "./_components/OverviewCards"
 
 export const metadata = {
   title: "Overview - dashboard carzone"
 }
-
-const orders = [
-  {
-    order: "INV001",
-    customer: "salman",
-    date: "2024-06-23",
-    amount: "IDR. 120M",
-    status: "Not paid",
-    statusVariant: "destructive"
-  },
-  {
-    order: "INV002",
-    customer: "rifki",
-    date: "2024-06-24",
-    amount: "IDR. 120M",
-    status: "paid",
-    statusVariant: "success"
-  },
-  {
-    order: "INV003",
-    customer: "rayhan",
-    date: "2024-06-25",
-    amount: "IDR. 120M",
-    status: "paid",
-    statusVariant: "success"
-  },
-  {
-    order: "INV004",
-    customer: "afif",
-    date: "2024-06-26",
-    amount: "IDR. 120M",
-    status: "paid",
-    statusVariant: "success"
-  }
-];
-
 
 const DashboardPage = () => {
   return (
@@ -56,33 +20,9 @@ const DashboardPage = () => {
           <CardTitle className="text-4xl">IDR. 984M</CardTitle>
         </CardHeader>
       </Card>
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription>Available Cars</CardDescription>
-          <CardTitle className="text-4xl">12 units</CardTitle>
-        </CardHeader>
-        <CardFooter className="mt-6">
-          <Link className="btn btn-outline" href="/dashboard/cars">View Table</Link>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription>Customers</CardDescription>
-          <CardTitle className="text-4xl">+54</CardTitle>
-        </CardHeader>
-        <CardFooter className="mt-6">
-          <Link className="btn btn-outline" href="/dashboard/users">View Table</Link>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader className="pb-2">
-          <CardDescription>Brands</CardDescription>
-          <CardTitle className="text-4xl">4 brands</CardTitle>
-        </CardHeader>
-        <CardFooter className="mt-6">
-          <Link className="btn btn-outline" href="/dashboard/brands">View Table</Link>
-        </CardFooter>
-      </Card>
+      <CarsOverview />
+      <UsersOverview />
+      <BrandsOverview />
     </div>
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <div className="col-span-2 md:col-span-3 lg:col-span-2">
@@ -101,30 +41,7 @@ const DashboardPage = () => {
           </div>
         </CardHeader>
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Order</TableHead>
-                <TableHead>Customer</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead>Amount</TableHead>
-                <TableHead>Status</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {orders.map((item, i) => (
-                <TableRow key={i}>
-                  <TableCell className="font-medium">{item.order}</TableCell>
-                  <TableCell>{item.customer}</TableCell>
-                  <TableCell>{item.date}</TableCell>
-                  <TableCell>{item.amount}</TableCell>
-                  <TableCell>
-                    <Badge variant={item.statusVariant}>{item.status}</Badge>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <OrdersTable />
         </CardContent>
       </Card>
     </div>
